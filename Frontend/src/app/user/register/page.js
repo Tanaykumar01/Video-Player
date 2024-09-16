d1e7e550760f09f3044a8bf8d6f18d2c27d28813
Tanaykumar01 <tanayjhunjhunwala.official@gmail.com>
2024-09-17 01:43:36 +0530

    Refactor registration page to include username and avatar fields

--- Frontend/src/app/user/register/page.js
+++ Frontend/src/app/user/register/page.js
@@ -5,20 +5,35 @@ import { useRouter } from 'next/navigation';
 
 export default function Register() {
   const [name, setName] = useState('');
+  const [username, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [image, setImage] = useState(null);
   const router = useRouter();
 
-  const handleRegister = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-    // Mock registration logic, replace with real authentication
-    if (password !== confirmPassword) {
-      alert('Passwords do not match');
+    if(password !== confirmPassword){
+      alert("Passwords do not match");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("fullName" , name);
+    formData.append("username" , username);
+    formData.append('email', email);
+    formData.append('password', password);
+    if(image) formData.append("avatar", image);
+    const response = await fetch('http://localhost:8000/api/v1/users/register', {
+      method: 'POST',
+      body: formData, 
+    });
+    const data = await response.json();
+    if (data.statusCode !== 200) {
+      alert('Passwords is not strong enough');
     } else {
-      // Simulate successful registration, replace with actual logic
       alert('Registration successful');
-      router.push('/login'); // Redirect to the login page after successful registration
+      router.push('/user/login');
     }
   };
 
@@ -39,6 +54,20 @@ export default function Register() {
                 required
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+                className="w-full px-3 py-2 mt-1 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                User Name
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                required
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
                 className="w-full px-3 py-2 mt-1 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               />
             </div>
@@ -84,8 +113,22 @@ export default function Register() {
                 className="w-full px-3 py-2 mt-1 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
               />
             </div>
+            <div className="mb-6">
+              <label className="block text-sm font-medium text-gray-700">
+                Avatar
+              </label>
+              <input
+                id="avatar"
+                name="avatar"
+                type="file"
+                accept="image/*"
+                required
+                onChange={(e) => setImage(e.target.files[0])}
+                className="w-full px-3 py-2 mt-1 text-gray-700 bg-gray-50 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
           </div>
-
+          
           <button
             type="submit"
             className="w-full px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
